5422d24efb988bc055e2601fa6626e3a3f361eab
MayurThaware122 <2023bit002@sggs.ac.in>
2025-04-20 17:45:24 +0530

    TextAnalyzer

---
+++ Practical15/src/practical15/Practical15.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practical15;
+
+/**
+ *
+ * @author mayur
+ */
+public class Practical15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new SudokuGUI().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
